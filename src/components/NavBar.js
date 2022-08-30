ad41c48c5db5925f5f13c58d65a9d7e827a171b9
Nicolas Noejovich <noejovichn@gmail.com>
2022-08-30 17:28:20 -0300

    hola

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import App from "../App.css";
+
+const NavBar = () => {
+    return(
+        <ul className="navbar"> 
+            <li><a className="deco" href="#">HOME</a></li>
+            <li><a className="deco" href="#">SHOP</a></li>
+            <li><a className="deco" href="#">ABOUT US</a></li>
+            <li><a className="deco" href="#">CONTACT</a></li>
+   </ul>
+        
+    )
+}
+
+export default NavBar;
+
